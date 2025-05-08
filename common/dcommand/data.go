91932f3507ad94a687a996ba3106ac1d50724746
Rhyker Wells <a.rhykerw@gmail.com>
2025-05-08 20:53:21 +0000

    docs(dcommand): add comments for clarity

--- common/dcommand/data.go
+++ common/dcommand/data.go
@@ -5,13 +5,13 @@ import "github.com/bwmarrin/discordgo"
 type Data struct {
 	Session *discordgo.Session
 
-	GuildID   string
-	ChannelID string
-	Author    *discordgo.User
+	GuildID   string // GuildID of the executed command
+	ChannelID string // ChannelID the command was executed in
+	Author    *discordgo.User // User object of the triggering user
 
-	Message        *discordgo.Message
-	Args           []string
-	ArgsNotLowered []string
+	Message        *discordgo.Message // Message object of the triggering message
+	Args           []string // Lowered args parsed to commands
+	ArgsNotLowered []string // Args parsed to command
 
 	Handler *CommandHandler
 }
