b7c7ca97504cf0f15e6d01687009fb3121262404
Rhyker Wells <a.rhykerw@gmail.com>
2025-05-12 22:34:27 +0000

    feat(frontend): Add polished modlog JS

---
+++ frontend/static/js/saveModlog.js
@@ -0,0 +1,44 @@
+document.getElementById('saveModlogButton').addEventListener('click', function() {
+	const input = document.getElementById('modlogInput').value.trim();
+	document.getElementById('successAlert').style.display = 'block';
+	document.getElementById('successAlert').style.opacity = 1; // Fade in the alert
+	const baseurl = document.body.getAttribute('data-url');
+	const guildID = document.body.getAttribute('data-guild-id');
+	const url = `${baseurl}/dashboard/${guildID}/manage/update-channel`;
+	fetch(url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ channel: input })
+	})
+	.catch((error) => {
+		console.error('Error:', error);
+	});
+	setTimeout(function() {
+		document.getElementById('successAlert').style.opacity = 0;
+		setTimeout(function() {
+			document.getElementById('successAlert').style.display = 'none';
+		}, 300);
+	}, 3000);
+});
+document.addEventListener('DOMContentLoaded', function () {
+	const channelItems = document.querySelectorAll('.channelListItem');
+	const modLogLabel = document.getElementById('modlogDropdownLabel');
+	const modlogHiddenInput = document.getElementById('modlogInput');
+	channelItems.forEach(item => {
+		item.addEventListener('click', function (e) {
+			e.preventDefault();
+			const value = this.getAttribute('data-value');
+			const text = this.textContent;
+			modLogLabel.textContent = text;
+			modlogHiddenInput.value = value;
+		});
+	});
+	var modlogID = "{{.CurrentGuild.Modlog}}"; 
+	var selectedItem = document.querySelector(`a[data-value="${modlogID}"]`);
+	if (selectedItem) {
+		document.getElementById("modlogDropdownLabel").textContent = selectedItem.textContent;
+		selectedItem.classList.add('selected');
+	}
+});
