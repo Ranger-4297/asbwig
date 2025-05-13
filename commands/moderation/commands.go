92cd162e6e4589c013b8f9e62376659169f162e3
Rhyker Wells <a.rhykerw@gmail.com>
2025-05-13 23:57:17 +0000

    feat(moderation): Add helper functions

--- commands/moderation/commands.go
+++ commands/moderation/commands.go
@@ -1,13 +1,55 @@
 package moderation
 
 import (
+	"context"
 	"strings"
 	"time"
 
+	"slices"
+
 	"github.com/RhykerWells/asbwig/bot/functions"
-	"github.com/RhykerWells/asbwig/commands/util"
+	"github.com/RhykerWells/asbwig/commands/moderation/models"
+	"github.com/RhykerWells/asbwig/common"
 	"github.com/RhykerWells/asbwig/common/dcommand"
+	"github.com/bwmarrin/discordgo"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+	"github.com/volatiletech/sqlboiler/v4/types"
 )
+
+func isEnabled(guildID string) bool {
+	config, _ := models.ModerationConfigs(qm.Where("guild_id=?", guildID)).One(context.Background(), common.PQ)
+	return config.Enabled
+}
+
+func requireRoles(guildID, roleRequirements string) types.StringArray {
+	config, _ := models.ModerationConfigs(qm.Where("guild_id=?", guildID)).One(context.Background(), common.PQ)
+	var requiredRoles types.StringArray
+	switch roleRequirements{
+	case "warn":
+		requiredRoles = config.RequiredWarnRoles
+	case "mute":
+		requiredRoles = config.RequiredMuteRoles
+	case "unmute":
+		requiredRoles = config.RequiredUnmuteRoles
+	case "kick":
+		requiredRoles = config.RequiredKickRoles
+	case "ban":
+		requiredRoles = config.RequiredBanRoles
+	case "unban":
+		requiredRoles = config.RequiredUnbanRoles
+	}
+	return requiredRoles
+}
+
+func hasCommandPermissions(guildID string, user *discordgo.Member, requireType string) bool {
+	requiredRoles := requireRoles(guildID, requireType)
+	for _, role := range user.Roles {
+		if slices.Contains(requiredRoles, role) {
+			return true
+		}
+	}
+	return false
+}
 
 var warnCommand = &dcommand.AsbwigCommand{
 	Command:     "warn",
@@ -19,14 +61,37 @@ var warnCommand = &dcommand.AsbwigCommand{
 		{Name: "Reason", Type: dcommand.String},
 	},
 	ArgsRequired: 2,
-	Run: util.AdminOrManageServerCommand(func(data *dcommand.Data) {
+	Run: func(data *dcommand.Data) {
+		enabled := isEnabled(data.GuildID)
+		if !enabled {
+			functions.DeleteMessage(data.ChannelID, data.Message.ID, 1*time.Second)
+			functions.SendBasicMessage(data.ChannelID, "The moderation system has not been enabled please enable it on the dashboard.", 30*time.Second)
+			return
+		}
 		author, _ := functions.GetMember(data.GuildID, data.Author.ID)
+		ok := hasCommandPermissions(data.GuildID, author, "warn")
+		if !ok {
+			functions.DeleteMessage(data.ChannelID, data.Message.ID, 1*time.Second)
+			functions.SendBasicMessage(data.ChannelID, "You don't have the required roles for this command.", 30*time.Second)
+			return
+		}
 		target, _ := functions.GetMember(data.GuildID, data.Args[0])
+		if target.User.ID == author.User.ID {
+			functions.DeleteMessage(data.ChannelID, data.Message.ID, 1*time.Second)
+			functions.SendBasicMessage(data.ChannelID, "You can't warn yourself.", 30*time.Second)
+			return
+		}
+		ok = functions.IsMemberHigher(data.GuildID, author, target)
+		if !ok {
+			functions.DeleteMessage(data.ChannelID, data.Message.ID, 1*time.Second)
+			functions.SendBasicMessage(data.ChannelID, "You don't have the correct permissions to warn this user (target has higher role).")
+			return
+		}
 		warnReason := strings.Join(data.Args[1:], " ")
 		err := logCase(data.GuildID, author, target, logWarn, data.ChannelID, warnReason)
 		if err != nil {
 			functions.SendBasicMessage(data.ChannelID, "Please setup a modlog channel before running this command", 30*time.Second)
 			return
 		}
-	}),
+	},
 }
