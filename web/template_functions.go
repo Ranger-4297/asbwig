2adb856ce83b754a7692ec566b2c9ed45f07d4b8
Rhyker Wells <a.rhykerw@gmail.com>
2025-05-13 15:48:45 +0000

    feat(web): add template_functions
    Util functions for text/template generation

---
+++ web/template_functions.go
@@ -0,0 +1,47 @@
+package web
+
+import (
+	"encoding/json"
+	"html/template"
+)
+
+func dict(pairs ...interface{}) map[int]interface{} {
+	result := make(map[int]interface{})
+	for i := 0; i < len(pairs); i += 2 {
+		key, _ := pairs[i].(int)
+		result[key] = pairs[i+1]
+	}
+	return result
+}
+
+
+func seq(start, end int) []int {
+	var result []int
+	for i := start; i <= end; i++ {
+		result = append(result, i)
+	}
+	return result
+}
+
+func inSlice(val string, slice interface{}) bool {
+	switch s := slice.(type) {
+	case []string:
+		for _, item := range s {
+			if item == val {
+				return true
+			}
+		}
+	case []interface{}:
+		for _, item := range s {
+			if str, ok := item.(string); ok && str == val {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func toJson(v interface{}) template.JS {
+	b, _ := json.Marshal(v)
+	return template.JS(b)
+}
